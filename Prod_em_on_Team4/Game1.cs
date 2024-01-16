cd3dd18cf6c0d52e6c716c852033cbe0215fd1bb
Arinze Ezeike <12EzeikeA@student.smchull.org>
2024-01-16 11:03:56 +0000

    merge

--- Prod_em_on_Team4/Game1.cs
+++ Prod_em_on_Team4/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
 
 namespace Prod_em_on_Team4
 {
@@ -8,25 +10,35 @@ namespace Prod_em_on_Team4
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Player firstPlayer;
+
+        public static int screenWidth;
+        public static int screenHeight;
+        public static float gravityAmount = 1;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 1000;
+            _graphics.PreferredBackBufferHeight = 800;
+
+            screenWidth = _graphics.PreferredBackBufferWidth;
+            screenHeight = _graphics.PreferredBackBufferHeight;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            firstPlayer = new Player(new Rectangle(), new Vector2(_graphics.PreferredBackBufferWidth / 2, 0), Color.AliceBlue);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            firstPlayer.LoadContent(Content, "player2.0");
             // TODO: use this.Content to load your game content here
         }
 
@@ -34,8 +46,10 @@ namespace Prod_em_on_Team4
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            //Debug.Write(Keyboard.GetState().IsKeyDown(Keys.Right)); Debug.Write(Keyboard.GetState().IsKeyDown(Keys.Left));
+            firstPlayer.Update(gameTime, true);
             // TODO: Add your update logic here
+            //hi
 
             base.Update(gameTime);
         }
@@ -43,6 +57,10 @@ namespace Prod_em_on_Team4
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            firstPlayer.Draw(_spriteBatch);
+            _spriteBatch.End();
+
 
             // TODO: Add your drawing code here
 
---
+++ Prod_em_on_Team4/Player.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using System.Diagnostics;
+
+namespace Prod_em_on_Team4
+{
+    internal class Player : Sprite
+    {
+        private bool moveLeft;
+        private bool moveRight;
+        private bool moveUp;
+
+        private float playerYVelocity = 0;
+        private float jumpAmount = 20;
+
+        public Player() : base()
+        { }
+
+        public Player(Rectangle boundingBox, Vector2 spritePosition, Color spriteColour)
+            : base(boundingBox, spritePosition, spriteColour)
+        {
+            _spritePosition = spritePosition;
+            _spriteColour = spriteColour;
+            _spriteBox = boundingBox;
+        }
+        public override void Update(GameTime gameTime, bool gameStarted)
+        {
+            _spritePosition = new Vector2(_spritePosition.X, _spritePosition.Y);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                moveLeft = true;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                moveRight = true;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                moveUp = true;
+            }
+
+            if (moveLeft == true && _spritePosition.X > 0)
+            {
+                _spritePosition.X -= 10;
+                moveLeft = false;
+            }
+
+            if (moveRight == true && _spritePosition.X < Game1.screenWidth - _spriteTexture.Width)
+            {
+                _spritePosition.X += 10;
+                moveRight = false;
+            }
+            //do stuff using the keyboard
+
+
+
+
+
+            if (_spritePosition.Y + playerYVelocity < Game1.screenHeight - _spriteTexture.Height)
+            {
+                _spritePosition.Y += playerYVelocity;
+                playerYVelocity += Game1.gravityAmount;
+            }
+            //adds gravity to the player
+            else
+            {
+                _spritePosition.Y = Game1.screenHeight - _spriteTexture.Height;
+                playerYVelocity = 0;
+                if (moveUp == true)
+                {
+                    playerYVelocity -= jumpAmount;
+                    moveUp = false;
+                }
+            }
+            //jumping code
+
+
+        }
+
+        /*public void LoadContent(ContentManager myContent)
+        {
+            myContent.RootDirectory = "Content";
+            _spriteTexture = myContent.Load<Texture2D>("player2.0");
+
+        }*/
+
+    }
+}
